cc0b269d05bd245b839b4fd9f1a216fc5a52b099
Matheus Castro <goca1377@gmail.com>
2018-08-24 17:26:49 -0300

    formulario login

--- src/login.js
+++ src/login.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
+import './login.css'
 import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 class Login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <Typography variant="display1">Login!</Typography>
+                <Divider className="size" />
+                <Typography variant="title" className="title">Sign in to Meu Tutor</Typography>
+                <Typography variant="subheading">Access your account to use the system</Typography>
+                <Divider className="size" />
+                <div className="formContainer">
+                    <TextField
+                        id="with-placeholder"
+                        label="Enter your email"
+                        placeholder="Placeholder"
+                        margin="normal"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        margin="normal"
+                    />
+                </div>
+                <div>
+                    <Button variant="contained" size="large" color="primary">
+                        Access
+                    </Button>
+                </div>
+                <Divider className="size" />
             </div>
         )
     }
