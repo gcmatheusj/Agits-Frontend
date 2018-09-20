2298e35532fa7c590005acc320b2d1dbc048be44
Gustavo D. J. Lima <gustavodl@outlook.com.br>
2018-09-20 11:25:30 -0300

    Stepper atualizado

--- src/components/novoTutor/Stepper.js
+++ src/components/novoTutor/Stepper.js
@@ -10,7 +10,7 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 import Start from "./Steps/Start"
-import DefineDomainAndSubject from './Steps/DefineDomain'
+import DefineDomainAndSubject from './Steps/DefineDomainAndSubject'
 
 const styles = theme => ({
     root: {
