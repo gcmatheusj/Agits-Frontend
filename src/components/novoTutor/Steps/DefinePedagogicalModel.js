68d18fc224912554e3984805bf679230f96ce17e
Italo <italojonas@hotmail.com>
2018-10-10 15:44:43 -0300

    updated-hover-steps

--- src/components/novoTutor/Steps/DefinePedagogicalModel.js
+++ src/components/novoTutor/Steps/DefinePedagogicalModel.js
@@ -50,7 +50,6 @@ class DefinePedagogicalModel extends React.Component {
             <div style={{ justifyContent: "center" }}>
                 <Typography variant="subheading">
                     We use the most advanced techniques for providing an adaptive and intelligent tutor for your students!
-<<<<<<< HEAD
             </Typography>
                 <CardActionArea>
                     <Paper className={classes.root}>
@@ -72,27 +71,6 @@ class DefinePedagogicalModel extends React.Component {
                                             given time.
                             </Typography>
                                     </Grid>
-=======
-                </Typography>
-                <Paper className={classes.root}>
-                    <Grid container spacing={16} style={{alignItems: "center"}}>
-                        <Grid item>
-                            <ButtonBase className={classes.image}>
-                                <img className={classes.img} alt="complex" src={curriculum} />
-                            </ButtonBase>
-                        </Grid>
-                        <Grid container item xs={12} sm container>
-                            <Grid item xs container direction="column" spacing={16}>
-                                <Grid item xs>
-                                    <Typography gutterBottom variant="title">
-                                        Knowledge tracing and curriculum sequence
-                                    </Typography>
-                                    <Typography gutterBottom variant="subheading" align="justify">
-                                        By default your custom tutor makes use of current intelligent techniques that model
-                                        student's knowledge to accurately decide which problems it should give to students at any
-                                        given time.
-                                    </Typography>
->>>>>>> 0fd577d27cc0aae884476f36b35532a65b97598c
                                 </Grid>
                             </Grid>
                             <Grid justify="center" alignItems="center" >
@@ -104,10 +82,8 @@ class DefinePedagogicalModel extends React.Component {
                                 />
                             </Grid>
                         </Grid>
-<<<<<<< HEAD
                     </Paper>
                 </CardActionArea>
-                <div style={{marginTop: 10}}/>
                 <CardActionArea>
                     <Paper className={classes.root}>
                         <Grid container spacing={16} style={{ alignItems: "center" }}>
@@ -127,35 +103,6 @@ class DefinePedagogicalModel extends React.Component {
                                             your students will learn using the tutor
                             </Typography>
                                     </Grid>
-=======
-                        <Grid justify="center" alignItems="center" >
-                            <Switch
-                                checked={this.state.checkedA}
-                                onChange={this.handleChange('checkedA')}
-                                value="checkedA"
-                                color="primary"
-                            />
-                        </Grid>
-                    </Grid>
-                </Paper>
-                <Paper className={classes.root}>
-                    <Grid container spacing={16} style={{alignItems: "center"}}>
-                        <Grid item>
-                            <ButtonBase className={classes.image}>
-                                <img className={classes.img} alt="complex" src={syllabus} />
-                            </ButtonBase>
-                        </Grid>
-                        <Grid container item xs={12} sm container>
-                            <Grid item xs container direction="column" spacing={16}>
-                                <Grid item xs>
-                                    <Typography gutterBottom variant="title">
-                                       Syllabus
-                                    </Typography>
-                                    <Typography gutterBottom variant="subheading" align="justify">
-                                        Enable this feature if you want to manage the schedule and subjects on which
-                                        your students will learn using the tutor
-                                    </Typography>
->>>>>>> 0fd577d27cc0aae884476f36b35532a65b97598c
                                 </Grid>
                             </Grid>
                             <Grid justify="center" alignItems="center" >
