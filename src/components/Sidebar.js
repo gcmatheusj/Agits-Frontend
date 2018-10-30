7f2e38065443756be41094fe36ca161e45d1bfb5
Gustavo D. J. Lima <gustavodl@outlook.com.br>
2018-10-29 22:18:04 -0300

    padrão atualizado

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,87 +1,72 @@
-import React, { Component } from "react";
-import { Typography, Grid, Button } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import PropTypes from "prop-types";
+import React from 'react';
+import { Typography, Grid, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-import Background from "../assets/imagem_fundo_filtro.png";
-import logo from "../assets/meu-tutor.png";
+import Background from '../assets/imagem_fundo_filtro.png';
+import logo from '../assets/meu-tutor.png';
 
 const styles = theme => ({
   root: {
-    backgroundImage: "url(" + Background + ")",
-    backgroundSize: "cover",
-    height: "100vh",
-    [theme.breakpoints.down("sm")]: {
-      display: "none"
+    backgroundImage: `url(${Background})`,
+    backgroundSize: 'cover',
+    height: '100vh',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
     },
-    userSelect: "none"
+    userSelect: 'none',
   },
   grid: {
-    alignItems: "center"
+    alignItems: 'center',
   },
   button: {
-    borderColor: "#ff572f",
-    color: "#ff572f",
-    verticalAlign: "middle",
-    border: "2px solid",
-    padding: "1px"
+    borderColor: '#ff572f',
+    color: '#ff572f',
+    verticalAlign: 'middle',
+    border: '2px solid',
+    padding: '1px',
   },
   content: {
     margin: 70,
-    color: "white",
-    userSelect: "none"
-  }
+    color: 'white',
+    userSelect: 'none',
+  },
 });
 
-class Sidebar extends Component {
-  /*  (useless constructor)
-        constructor(props) {
-            super(props)
-        }
-    */
-  render() {
-    const { classes } = this.props;
-    return (
-      <Grid className={classes.root} container>
-        <Grid
-          className={classes.grid}
-          container
-          justify="center"
-          direction="row"
-        >
-          <div className="container">
-            <div className="content">
-              <div align="center">
-                <img src={logo} alt="" />
-              </div>
-              <Typography
-                className={classes.content}
-                variant="h4"
-                align="center"
+function Sidebar(props) {
+  const { classes } = props;
+
+  return (
+    <Grid className={classes.root} container>
+      <Grid className={classes.grid} container justify="center" direction="row">
+        <div className="container">
+          <div className="content">
+            <div align="center">
+              <img src={logo} alt="" />
+            </div>
+            <Typography className={classes.content} variant="h4" align="center">
+              LOREM IPSUM PORTTITOR AENEAN TACITI SCELERISQUE TEMPUS DONEC.
+            </Typography>
+            <div align="center">
+              <Button
+                id="btn-ln"
+                className={classes.button}
+                variant="outlined"
+                size="large"
+                color="primary"
               >
-                LOREM IPSUM PORTTITOR AENEAN TACITI SCELERISQUE TEMPUS DONEC.
-              </Typography>
-              <div align="center">
-                <Button
-                  id="btn-ln"
-                  className={classes.button}
-                  variant="outlined"
-                  size="large"
-                  color="primary"
-                >
-                  Learn More
-                </Button>
-              </div>
+                Learn More
+              </Button>
             </div>
           </div>
-        </Grid>
+        </div>
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
 
 Sidebar.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.shape.isRequired,
 };
 
 export default withStyles(styles)(Sidebar);
