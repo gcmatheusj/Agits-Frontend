1b7cfa0f2742b1f1946157ea5e0064c0601fce20
Gustavo D. J. Lima <gustavodl@outlook.com.br>
2018-10-29 22:44:13 -0300

    padrão atualizado

--- src/components/NewHeader/index.js
+++ src/components/NewHeader/index.js
@@ -1,133 +1,149 @@
-import React, { Component, Fragment } from 'react'
-import { AppBar, Toolbar, IconButton, Typography, Hidden, Drawer, Divider, CssBaseline, Card } from '@material-ui/core'
-import { withStyles } from '@material-ui/core/styles'
-import { Menu } from '@material-ui/icons'
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Hidden,
+  Drawer,
+  Divider,
+  CssBaseline,
+  Card,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { Menu } from '@material-ui/icons';
 
 const drawerWidth = 240;
 
 const styles = theme => ({
-    root: {
-        display: 'flex',
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
     },
-    drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
     },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-    },
-    menuButton: {
-        marginRight: 20,
-        [theme.breakpoints.up('sm')]: {
-            display: 'none',
-        },
-    },
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing.unit * 3,
-    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
 });
 
 class NovoHeader extends Component {
+  state = {
+    mobileOpen: false,
+  };
 
-    state = {
-        mobileOpen: false,
-    };
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  };
 
-    handleDrawerToggle = () => {
-        this.setState(state => ({ mobileOpen: !state.mobileOpen }));
-    };
+  render() {
+    const { classes, container } = this.props;
+    const { mobileOpen } = this.state;
 
-    render() {
-        const { classes } = this.props;
-        const { mobileOpen } = this.state
+    const drawer = (
+      <div>
+        <Hidden smDown>
+          <div className={classes.toolbar} />
+        </Hidden>
+        <Divider />
+        Hello Drawer
+      </div>
+    );
 
-        const drawer = (
-            <div>
-                <Hidden smDown>
-                    <div className={classes.toolbar} />
-                </Hidden>
-                <Divider />
-                Hello Drawer
-            </div>
-        );
-
-        return (
-            <Fragment>
-                <CssBaseline />
-                <div className={classes.root}>
-                    <AppBar position="fixed" className={classes.appBar}>
-                        <Toolbar>
-                            <IconButton
-                                color="inherit"
-                                aria-label="Open drawer"
-                                onClick={this.handleDrawerToggle}
-                                className={classes.menuButton}
-                            >
-                                <Menu />
-                            </IconButton>
-                            <Typography variant="h6" color="inherit" noWrap>
-                                Responsive drawer
-                      </Typography>
-                        </Toolbar>
-                    </AppBar>
-                    <nav className={classes.drawer}>
-                        {/* The implementation can be swap with js to avoid SEO duplication of links. */}
-                        <Hidden smUp implementation="css">
-                            <Drawer
-                                container={this.props.container}
-                                variant="temporary"
-                                open={mobileOpen}
-                                onClose={this.handleDrawerToggle}
-                                classes={{
-                                    paper: classes.drawerPaper,
-                                }}
-                                ModalProps={{
-                                    keepMounted: true, // Better open performance on mobile.
-                                }}
-                            >
-                                {drawer}
-                            </Drawer>
-                        </Hidden>
-                        <Hidden xsDown implementation="css">
-                            <Drawer
-                                classes={{
-                                    paper: classes.drawerPaper,
-                                }}
-                                variant="permanent"
-                                open
-                            >
-                                {drawer}
-                            </Drawer>
-                        </Hidden>
-                    </nav>
-                    <main className={classes.content}>
-                        <div className={classes.toolbar} />
-                        <Card style={{ padding: 10 }}>
-                            <Typography paragraph>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
-                                elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-                                hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-                                velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-                                Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-                                viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
-                                Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
-                                at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-                                ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-                            </Typography>
-                        </Card>
-                    </main>
-                </div>
-            </Fragment>
-        )
-    }
+    return (
+      <Fragment>
+        <CssBaseline />
+        <div className={classes.root}>
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.menuButton}
+              >
+                <Menu />
+              </IconButton>
+              <Typography variant="h6" color="inherit" noWrap>
+                Responsive drawer
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <nav className={classes.drawer}>
+            {/* The implementation can be swap with js to avoid SEO duplication of links. */}
+            <Hidden smUp implementation="css">
+              <Drawer
+                container={container}
+                variant="temporary"
+                open={mobileOpen}
+                onClose={this.handleDrawerToggle}
+                classes={{
+                  paper: classes.drawerPaper,
+                }}
+                ModalProps={{
+                  keepMounted: true, // Better open performance on mobile.
+                }}
+              >
+                {drawer}
+              </Drawer>
+            </Hidden>
+            <Hidden xsDown implementation="css">
+              <Drawer
+                classes={{
+                  paper: classes.drawerPaper,
+                }}
+                variant="permanent"
+                open
+              >
+                {drawer}
+              </Drawer>
+            </Hidden>
+          </nav>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Card style={{ padding: 10 }}>
+              <Typography paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
+                elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+                hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
+                velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
+                Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
+                quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin
+                fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
+                lobortis feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+                faucibus et molestie ac.
+              </Typography>
+            </Card>
+          </main>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-export default withStyles(styles)(NovoHeader)
+NovoHeader.propTypes = {
+  classes: PropTypes.shape.isRequired,
+  container: PropTypes.shape.isRequired,
+};
+
+export default withStyles(styles)(NovoHeader);
