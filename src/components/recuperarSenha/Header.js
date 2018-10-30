2d1342bfbae3af5af0bbae1ff980fa660427ae1e
Gustavo D. J. Lima <gustavodl@outlook.com.br>
2018-10-29 22:28:27 -0300

    padrão atualizado

--- src/components/recuperarSenha/Header.js
+++ src/components/recuperarSenha/Header.js
@@ -1,58 +1,59 @@
-import React, { Component } from 'react'
-//import { AppBar, Toolbar, IconButton, Typography, Button, Grid } from '@material-ui/core' (unused)
-import { AppBar, Toolbar, Typography, Button, Grid } from '@material-ui/core'
-//import MenuIcon from '@material-ui/icons/Menu' (unused)
-import logo from '../../assets/meu-tutor.png'
-import { withStyles } from '@material-ui/core/styles'
-import PropTypes from 'prop-types'
+import React from 'react';
+import {
+  AppBar, Toolbar, Typography, Button, Grid,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import logo from '../../assets/meu-tutor.png';
 
 const styles = theme => ({
-    root: {
-        backgroundColor: '#003175'
+  root: {
+    backgroundColor: '#003175',
+  },
+  logo: {
+    marginTop: 5,
+    width: 130,
+    [theme.breakpoints.down('sm')]: {
+      width: 110,
     },
-    logo: {
-        marginTop: 5,
-        width: 130,
-        [theme.breakpoints.down('sm')]: {
-            width: 110
-        },
-    }, 
-    button: {
-        backgroundColor: '#ff572f',
-        color: 'white'
+  },
+  button: {
+    backgroundColor: '#ff572f',
+    color: 'white',
+  },
+  label: {
+    marginTop: 8,
+    marginRight: 20,
+    fontSize: 18,
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
     },
-    label: {
-        marginTop: 8,
-        marginRight: 20,
-        fontSize: 18,
-        [theme.breakpoints.down('sm')]: {
-            display: 'none'
-        }
-    }
-})
+  },
+});
 
-class Header extends Component {
-    render() {
-        const { classes } = this.props
-        return (
-            <AppBar className={classes.root} position="static">
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" >
-                        <img className={classes.logo} src={logo} alt=""></img>
-                    </Typography>
-                    <Grid container justify="flex-end">
-                        <Typography className={classes.label} variant="subtitle1" color="inherit">
-                            Don't have an account?
-                        </Typography>
-                        <Button className={classes.button} size="small" variant="contained">Get Started</Button>
-                    </Grid>
-                </Toolbar>
-            </AppBar>
-        )
-    }
+function Header(props) {
+  const { classes } = props;
+
+  return (
+    <AppBar className={classes.root} position="static">
+      <Toolbar>
+        <Typography variant="h6" color="inherit">
+          <img className={classes.logo} src={logo} alt="" />
+        </Typography>
+        <Grid container justify="flex-end">
+          <Typography className={classes.label} variant="subtitle1" color="inherit">
+            Don&apos;t have an account?
+          </Typography>
+          <Button className={classes.button} size="small" variant="contained">
+            Get Started
+          </Button>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 Header.propTypes = {
-    classes: PropTypes.object.isRequired,
-}
-export default withStyles(styles)(Header)
+  classes: PropTypes.shape.isRequired,
+};
+export default withStyles(styles)(Header);
--- src/components/recuperarSenha/RecuperarSenha.js
+++ src/components/recuperarSenha/RecuperarSenha.js
@@ -1,131 +1,136 @@
-import React, { Component } from 'react'
-import { Typography, Divider, TextField, Button, Card, CardActions, CardContent, Grid} from '@material-ui/core'
-import { withStyles } from '@material-ui/core/styles'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import {
+  Typography,
+  Divider,
+  TextField,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 const styles = theme => ({
-    grid: {
-        flexGrow: 1,
-        alignItems: "center",
-        [theme.breakpoints.down('sm')]: {
-            alignItems: "flex-start",
-            marginTop: 30,
-            marginLeft: 16,
-            marginRight: 16
-        },
+  grid: {
+    flexGrow: 1,
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      alignItems: 'flex-start',
+      marginTop: 30,
+      marginLeft: 16,
+      marginRight: 16,
     },
-    card: {
-        maxWidth: 550,  
-    },
-    title: {
-        fontSize: 27,
-        marginTop: 8,
-        fontWeight: 'bold'
-    },
-    subheading: {
-        marginBottom: 8
-    },
-    input: {
-        width: '100%'
-    },
-    cardActions: {
-        marginBottom: 10
-    },
-    formContainer: {
-        width: '100%',
-    },
-})
+  },
+  card: {
+    maxWidth: 550,
+  },
+  title: {
+    fontSize: 27,
+    marginTop: 8,
+    fontWeight: 'bold',
+  },
+  subheading: {
+    marginBottom: 8,
+  },
+  input: {
+    width: '100%',
+  },
+  cardActions: {
+    marginBottom: 10,
+  },
+  formContainer: {
+    width: '100%',
+  },
+});
 
 class RecuperarSenha extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cardNovaSenha: false,
-        };
-        this.onClick = this.onClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardNovaSenha: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
 
-    onClick() {
-        this.setState({cardNovaSenha:true});
-    }
+  onClick() {
+    this.setState({ cardNovaSenha: true });
+  }
 
-    render() {
-        const { classes } = this.props
-        return (
-            <Grid className={classes.grid} container direction="row" justify="center">
-                <Grid>
-                    <Card className={classes.card}>
-                        <CardContent >
-                            <Grid>
-                                <Grid className={classes.formContainer}>
-                                    <Typography align="center" variant="h6" className={classes.title}>
-                                        {(this.state.cardNovaSenha) ? "Enter the new password" : "Forgot your password?"}
-                                    </Typography>
-                                    <Typography align="center" variant="subtitle1" className={classes.subheading}>
-                                        {
-                                            (this.state.cardNovaSenha) ? 
-                                            "To recover your account, create a new password"
-                                            :
-                                            "To recover your password, enter your email below"
-                                        }
-                                    </Typography>
-                                    <Divider/>
-                                    <Grid>
-                                        {
-                                            (this.state.cardNovaSenha) ? 
-                                            [
-                                                <TextField
-                                                    className={classes.input}
-                                                    id="with-placeholder"
-                                                    label="Enter your new password" 
-                                                    margin="normal"
-                                                    key='0'
-                                                />,
-                                                <TextField
-                                                    className={classes.input}
-                                                    id="with-placeholder"
-                                                    label="Confirm your new password"
-                                                    margin="normal"
-                                                    key='1'
-                                                />
-                                            ]
-                                            :
-                                            <TextField
-                                                className={classes.input}
-                                                id="with-placeholder"
-                                                label="Email address"
-                                                margin="normal"
-                                            />
-                                        }
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                        </CardContent>
-                        <CardActions className={classes.cardActions}>
-                            <Grid container justify="flex-end">
-                                <Button size="medium">
-                                    Back to login
-                                </Button>
-                                <Button size="medium" variant="contained" color="primary" style={{backgroundColor: '#ff572f'}}
-                                        onClick={this.onClick}>
-                                    {
-                                        (this.state.cardNovaSenha) ?
-                                        "Change password"
-                                        :
-                                        "Send"
-                                    }
-                                </Button>
-                            </Grid>
-                        </CardActions>
-                    </Card>
+  render() {
+    const { classes } = this.props;
+    const { cardNovaSenha } = this.state;
+
+    return (
+      <Grid className={classes.grid} container direction="row" justify="center">
+        <Grid>
+          <Card className={classes.card}>
+            <CardContent>
+              <Grid>
+                <Grid className={classes.formContainer}>
+                  <Typography align="center" variant="h6" className={classes.title}>
+                    {cardNovaSenha ? 'Enter the new password' : 'Forgot your password?'}
+                  </Typography>
+                  <Typography align="center" variant="subtitle1" className={classes.subheading}>
+                    {cardNovaSenha
+                      ? 'To recover your account, create a new password'
+                      : 'To recover your password, enter your email below'}
+                  </Typography>
+                  <Divider />
+                  <Grid>
+                    {cardNovaSenha ? (
+                      [
+                        <TextField
+                          className={classes.input}
+                          id="with-placeholder"
+                          label="Enter your new password"
+                          margin="normal"
+                          key="0"
+                        />,
+                        <TextField
+                          className={classes.input}
+                          id="with-placeholder"
+                          label="Confirm your new password"
+                          margin="normal"
+                          key="1"
+                        />,
+                      ]
+                    ) : (
+                      <TextField
+                        className={classes.input}
+                        id="with-placeholder"
+                        label="Email address"
+                        margin="normal"
+                      />
+                    )}
+                  </Grid>
                 </Grid>
-            </Grid>
-        )
-    }
+              </Grid>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+              <Grid container justify="flex-end">
+                <Button size="medium">Back to login</Button>
+                <Button
+                  size="medium"
+                  variant="contained"
+                  color="primary"
+                  style={{ backgroundColor: '#ff572f' }}
+                  onClick={this.onClick}
+                >
+                  {cardNovaSenha ? 'Change password' : 'Send'}
+                </Button>
+              </Grid>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 RecuperarSenha.propTypes = {
-    classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.shape.isRequired,
+};
 
-export default withStyles(styles)(RecuperarSenha)
+export default withStyles(styles)(RecuperarSenha);
