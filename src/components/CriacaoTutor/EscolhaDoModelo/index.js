fbf30ba27d277949aa16f227830ea68dd497c5df
Henrique Couto <henrique.coutu@live.com>
2019-01-16 16:48:09 -0300

    demografico adicionado

--- src/components/CriacaoTutor/EscolhaDoModelo/index.js
+++ src/components/CriacaoTutor/EscolhaDoModelo/index.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 
 import Header from "../../Header"
 import Termo from '../../TermoConsentimento'
+import Demografico from '../../Demografico'
 
 import AlertDialogSlide from './Dialog'
 
@@ -37,6 +38,45 @@ class EscolhaDoModelo extends React.Component {
     complete: false,
     position: 0,
     checked: false,
+    respostas: {
+      activeStep: 0,
+      resposta1: null,
+      resposta2: null,
+      resposta3: null,
+      resposta4: null,
+      resposta5: null,
+      resposta6: null,
+      resposta7: null,
+      valorFerramenta1: 4,
+      valorFerramenta2: 4,
+      valorFerramenta3: 4,
+      valorFerramenta4: 4,
+      valorFerramenta5: 4,
+      valorFerramenta6: 4,
+      valorFerramenta7: 4,
+      valorFerramenta8: 4,
+      valorFerramenta9: 4,
+      valorFerramenta10: 4,
+      valorFerramenta11: 4,
+      valorFerramenta12: 4,
+      valorFerramenta13: 4,
+      valorFerramenta14: 4,
+      valorFerramenta15: 4,
+      valorFerramenta16: 4,
+      valorFerramenta17: 4,
+      valorFerramenta18: 4,
+      valorFerramenta19: 4,
+      valorFerramenta20: 4,
+      valorFerramenta21: 4,
+      valorFerramenta22: 4,
+      valorFerramenta23: 4,
+      valorFerramenta24: 4,
+      valorFerramenta25: 4,
+      valorFerramenta26: 4,
+      valor1: 5,
+      valor2: 5,
+      valor3: 5
+    }
   };
 
   handleChange = () => {
@@ -58,6 +98,43 @@ class EscolhaDoModelo extends React.Component {
   handleClose = () => {
     this.setState({ open: false, adress: '' });
   };
+
+  handleChangeDemografico(event) {
+    if (event.target.name === 'sexualidade') {
+      this.setState({ resposta1: event.target.value })
+      window.sessionStorage.setItem("resposta1", event.target.value)
+    } else if (event.target.name === 'idade') {
+      this.setState({ resposta2: event.target.value })
+      window.sessionStorage.setItem("resposta2", event.target.value)
+    } else if (event.target.name === 'ocupação') {
+      this.setState({ resposta3: event.target.value })
+      window.sessionStorage.setItem("resposta3", event.target.value)
+    } else if (event.target.name === 'escolaridade') {
+      this.setState({ resposta4: event.target.value })
+      window.sessionStorage.setItem("resposta4", event.target.value)
+    } else if (event.target.name === 'habilidades') {
+      this.setState({ resposta5: event.target.value })
+      window.sessionStorage.setItem("resposta5", event.target.value)
+    } else if (event.target.name === 'treinamento') {
+      this.setState({ resposta6: event.target.value })
+      window.sessionStorage.setItem("resposta6", event.target.value)
+    } else if (event.target.name === 'usoTecnologiasEducacionais') {
+      this.setState({ resposta7: event.target.value })
+      window.sessionStorage.setItem("resposta7", event.target.value)
+    }
+  }
+
+  next = () => {
+    if (this.state.position < 1) {
+      this.setState({
+        position: this.state.position+1
+      })
+    } else {
+      this.setState({
+        complete: true
+      })
+    }
+  }
 
   render() {
     const { classes } = this.props
@@ -101,12 +178,15 @@ class EscolhaDoModelo extends React.Component {
             open={this.state.open}
           />
 
-          <Dialog open={!complete} maxWidth='xl'>
+          <Dialog open={!complete} maxWidth='lg'>
             <DialogContent>
-              {position === 0 && <Termo checked={checked} handleChange={this.handleChange}/>}
+              {position === 0 ?
+                <Termo checked={checked} handleChange={this.handleChange} /> :
+                <Demografico respostas={this.state.respostas} onChangeValue={this.handleChangeDemografico}/>
+              }
             </DialogContent>
             <DialogActions>
-              <Button variant='contained' color='primary' disabled={!checked}>
+              <Button variant='contained' color='primary' disabled={!checked} onClick={this.next}>
                 Next
               </Button>
             </DialogActions>
---
+++ src/components/Demografico.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react'
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormControl from '@material-ui/core/FormControl'
+import FormLabel from '@material-ui/core/FormLabel'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    group: {
+        margin: `${theme.spacing.unit}px 0`,
+    },
+})
+
+class Demografico extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const { classes, respostas } = this.props
+        return (
+            <form onSubmit={(event) => this.props.onSubmitForm(event)}>
+                <Typography variant="display1" component="label" >
+                    Questionário Demográfico
+                </Typography>
+                <br /><div>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend">Inicialmente selecione seu gênero.</FormLabel>
+                        <RadioGroup
+                            aria-label="gender"
+                            name="sexualidade"
+                            className={classes.group}
+                            value={respostas.resposta1}
+                            onChange={this.props.onChangeValue}
+                        >
+                            <FormControlLabel value="Feminino" control={<Radio color="primary" />} label="Feminino" />
+                            <FormControlLabel value="Masculino" control={<Radio color="primary" />} label="Masculino" />
+                            <FormControlLabel value="Prefiro não responder" control={<Radio color="primary" />} label="Prefiro não responder" />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
+                <br /><div>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend">Por favor, informe sua idade.</FormLabel>
+                        <RadioGroup
+                            aria-label="gender"
+                            name="idade"
+                            value={respostas.resposta2}
+                            onChange={this.props.onChangeValue}
+                        >
+                            <FormControlLabel value="18 a 25 anos" control={<Radio color="primary" />} label="18 a 25 anos" />
+                            <FormControlLabel value="26 a 40 anos" control={<Radio color="primary" />} label="26 a 40 anos" />
+                            <FormControlLabel value="40 a 65 anos" control={<Radio color="primary" />} label="40 a 65 anos" />
+                            <FormControlLabel value="Mais de 65 anos
+                            " control={<Radio color="primary" />} label="Mais de 65 anos
+                            " />
+                            <FormControlLabel value="Prefiro não responder" control={<Radio color="primary" />} label="Prefiro não responder" />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
+                <br /><div>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend">Ocupação.</FormLabel>
+                        <RadioGroup
+                            aria-label="gender"
+                            name="ocupação"
+                            value={respostas.resposta3}
+                            onChange={this.props.onChangeValue}
+                        >
+                            <FormControlLabel value="Estudante" control={<Radio color="primary" />} label="Estudante" />
+                            <FormControlLabel value="Professor" control={<Radio color="primary" />} label="Professor" />
+                            <FormControlLabel value="Outra" control={<Radio color="primary" />} label="Outra" />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
+                <br /><div> 
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend">Qual o maior nível de educação que você completou?</FormLabel>
+                        <RadioGroup
+                            aria-label="gender"
+                            name="escolaridade"
+                            value={respostas.resposta4}
+                            onChange={this.props.onChangeValue}
+                        >
+                            <FormControlLabel value="Ensino Fundamental" control={<Radio color="primary" />} label="Ensino Fundamental" />
+                            <FormControlLabel value="Ensino Médio" control={<Radio color="primary" />} label="Ensino Médio" />
+                            <FormControlLabel value="Ensino Técnico" control={<Radio color="primary" />} label="Ensino Técnico" />
+                            <FormControlLabel value="Ensino Superior" control={<Radio color="primary" />} label="Ensino Superior" />
+                            <FormControlLabel value="Mestrado" control={<Radio color="primary" />} label="Mestrado" />
+                            <FormControlLabel value="Doutorado" control={<Radio color="primary" />} label="Doutorado" />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
+                <br /><div>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend">Como você classifica suas habilidades com a informática?</FormLabel>
+                        <RadioGroup
+                            aria-label="gender"
+                            name="habilidades"
+                            value={respostas.resposta5}
+                            onChange={this.props.onChangeValue}
+                        >
+                            <FormControlLabel value="Nenhuma habilidade" control={<Radio color="primary" />} label="Nenhuma habilidade" />
+                            <FormControlLabel value="Usuario iniciante" control={<Radio color="primary" />} label="Usuário iniciante" />
+                            <FormControlLabel value="Usuario intermediario" control={<Radio color="primary" />} label="Usuário intermediário" />
+                            <FormControlLabel value="Usuario avancado
+                            " control={<Radio color="primary" />} label="Usuário avançado
+                            " />
+                        </RadioGroup>
+                    </FormControl>
+                </div>        
+                <br /><div>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend">Você recebeu, durante sua formação, treinamento formal para o uso de tecnologias educacionais de informática?</FormLabel>
+                        <RadioGroup
+                            aria-label="gender"
+                            name="treinamento"
+                            value={respostas.resposta6}
+                            onChange={this.props.onChangeValue}
+                        >
+                            <FormControlLabel value="Sim" control={<Radio color="primary" />} label="Sim" />
+                            <FormControlLabel value="Não" control={<Radio color="primary" />} label="Não" />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
+                <br /><div>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend">Você se considera apto para o uso de tecnologias educacionais de informática?</FormLabel>
+                        <RadioGroup
+                            aria-label="gender"
+                            name="usoTecnologiasEducacionais"
+                            value={respostas.resposta7}
+                            onChange={this.props.onChangeValue}
+                        >
+                            <FormControlLabel value="Sim" control={<Radio color="primary" />} label="Sim" />
+                            <FormControlLabel value="Não" control={<Radio color="primary" />} label="Não" />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
+            </form>
+        )
+    }
+}
+
+Demografico.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Demografico)
