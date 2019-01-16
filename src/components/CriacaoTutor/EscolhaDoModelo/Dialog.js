5c553b3800179b46ba184c3ae065e3374bd9d0c7
Henrique Couto <henrique.coutu@live.com>
2019-01-16 16:04:28 -0300

    sad

--- src/components/CriacaoTutor/EscolhaDoModelo/Dialog.js
+++ src/components/CriacaoTutor/EscolhaDoModelo/Dialog.js
@@ -7,98 +7,74 @@ import Slide from '@material-ui/core/Slide';
 import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles';
 
-import Termo from '../../TermoConsentimento'
-
 import { Grid, Typography, TextField } from '@material-ui/core';
 
 function Transition(props) {
-  return <Slide direction="down" {...props} />;
+    return <Slide direction="down" {...props} />;
 }
 
 const styles = theme => ({
-  titleGrid: {
-    textAlign: 'left',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '1rem'
+    titleGrid: {
+        textAlign: 'left',
     },
-  },
-  textField: {
-    marginTop: 25,
-    width: '100%',
-  },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '1rem'
+        },
+    },
+    textField: {
+        marginTop: 25,
+        width: '100%',
+    },
 })
-
-const name = () => {
-  const { classes } = this.props
-  return (
-    <React.Fragment>
-      <Grid item xs={12} className={classes.titleGrid}>
-        <Typography className={classes.title} variant="subtitle1">
-          Name of your Tutor
-      </Typography>
-      </Grid>
-      <Grid item xs={12} >
-        <Divider />
-        <TextField
-          id="outlined-with-placeholder"
-          label="Input a name here..."
-          className={classes.textField}
-          margin="normal"
-          variant="outlined"
-        />
-      </Grid>
-    </React.Fragment>
-  )
-}
-
 
 class AlertDialogSlide extends React.Component {
 
-  state = {
-    position: 0,
-    checked: false,
-  }
 
-  handleChange = () => {
-    this.setState({
-      checked: !this.state.checked
-    })
-  }
+    render() {
 
-  render() {
+        const { classes } = this.props
 
-    const { classes } = this.props
-
-    return (
-      <Dialog
-        open={this.props.open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={this.props.handleClose}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
-        maxWidth='xl'
-      >
-        <DialogContent>
-          <Grid container spacing={16}>
-            <Termo checked={this.state.checked} handleChange={this.handleChange} />
-          </Grid>
-        </DialogContent>
-        <Grid style={{ padding: 24 }} container justify="flex-start">
-          <Button onClick={this.props.handleClose} style={{ marginRight: 10 }} >
-            Cancel
-          </Button>
-          <Button component={Link} to={this.props.adress} variant="contained" onClick={this.props.handleClose} color="primary" disabled={!this.state.checked}>
-            Next
-          </Button>
-        </Grid>
-      </Dialog>
-    )
-  }
+        return (
+            <Dialog
+                open={this.props.open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={this.props.handleClose}
+                aria-labelledby="alert-dialog-slide-title"
+                aria-describedby="alert-dialog-slide-description">
+                <DialogContent>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12} className={classes.titleGrid}>
+                            <Typography className={classes.title} variant="subtitle1">
+                                Name of your Tutor
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} >
+                            <Divider />
+                            <TextField
+                                id="outlined-with-placeholder"
+                                label="Input a name here..."
+                                className={classes.textField}
+                                margin="normal"
+                                variant="outlined"
+                            />
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <Grid style={{ padding: 24 }} container justify="flex-start">
+                    <Button onClick={this.props.handleClose} style={{ marginRight: 10 }} >
+                        Cancel
+                    </Button>
+                    <Button component={Link} to={this.props.adress} variant="contained" onClick={this.props.handleClose} color="primary">
+                        Next
+                    </Button>
+                </Grid>
+            </Dialog>
+        )
+    }
 }
 
 export default withStyles(styles)(AlertDialogSlide)
