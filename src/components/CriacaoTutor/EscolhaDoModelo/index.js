b4e2e276bf4b19c8f2c284a293c2df0fd527aae1
Henrique Couto <henrique.coutu@live.com>
2019-01-16 16:20:16 -0300

    termo de consentimento ajustado

--- src/components/CriacaoTutor/EscolhaDoModelo/index.js
+++ src/components/CriacaoTutor/EscolhaDoModelo/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { withStyles } from '@material-ui/core/styles'
-import { Grid, Typography, Card, CardContent, CardActionArea, Divider } from '@material-ui/core'
+import { Grid, Typography, Card, CardContent, CardActionArea, Divider, Dialog, DialogContent, DialogActions, Button } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
 import Header from "../../Header"
+import Termo from '../../TermoConsentimento'
 
 import AlertDialogSlide from './Dialog'
 
@@ -13,90 +14,112 @@ import scratch from '../../../assets/scratch2.png'
 import template from '../../../assets/template.png'
 
 const opcoesModelo = [
-    {
-        adress: '/passo-a-passo',
-        img: scratch,
-        title: 'Criar seu tutor do zero',
-        subtitle: 'Crie um novo tutor passo a passo e explore todos os recursos que trazemos para você.'
-    },
-    {
-        adress: '/usando-modelo',
-        img: template,
-        title: 'Usando modelo',
-        subtitle: 'Economize tempo usando tutor já configurado para uso! Mas fique à vontade para modificar.'
-    }
+  {
+    adress: '/passo-a-passo',
+    img: scratch,
+    title: 'Criar seu tutor do zero',
+    subtitle: 'Crie um novo tutor passo a passo e explore todos os recursos que trazemos para você.'
+  },
+  {
+    adress: '/usando-modelo',
+    img: template,
+    title: 'Usando modelo',
+    subtitle: 'Economize tempo usando tutor já configurado para uso! Mas fique à vontade para modificar.'
+  }
 ]
 
 class EscolhaDoModelo extends React.Component {
-    state = {
-        open: false,
-        redirect: false,
-        selectedModel: '',
-        adress: '#'
-    };
+  state = {
+    open: false,
+    redirect: false,
+    selectedModel: '',
+    adress: '#',
+    complete: false,
+    position: 0,
+    checked: false,
+  };
 
-    setRedirect = () => {
-        this.setState({
-            redirect: true
-        })
-    }
+  handleChange = () => {
+    this.setState({
+      checked: !this.state.checked
+    })
+  }
 
-    handleClickOpen = value => event => {
-        this.setState({ open: true, adress: value });
-    };
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
 
-    handleClose = () => {
-        this.setState({ open: false, adress: '' });
-    };
+  handleClickOpen = value => event => {
+    this.setState({ open: true, adress: value });
+  };
 
-    render() {
-        const { classes } = this.props
-        return (
-            <div className={classes.root}>
-                <Header title="AGITS">
-                    <Typography className={classes.title} variant="subtitle1">
-                        Create Tutor
+  handleClose = () => {
+    this.setState({ open: false, adress: '' });
+  };
+
+  render() {
+    const { classes } = this.props
+    const { complete, position, checked } = this.state
+    return (
+      <div className={classes.root}>
+        <Header title="AGITS">
+          <Typography className={classes.title} variant="subtitle1">
+            Create Tutor
                     </Typography>
-                    <Divider />
-                    <Typography className={classes.subtitle} variant="subtitle1">
-                        You can start by selecting create tutor from scratch or using template.
+          <Divider />
+          <Typography className={classes.subtitle} variant="subtitle1">
+            You can start by selecting create tutor from scratch or using template.
                     </Typography>
-                    <Grid container
-                        className={classes.container}
-                        justify="center"
-                        alignItems="center">
+          <Grid container
+            className={classes.container}
+            justify="center"
+            alignItems="center">
 
-                        {opcoesModelo.map((v, k) => {
-                            return (
-                                <Card key={k} className={classes.card}>
-                                    <CardActionArea className={classes.cardAction} value="teste" onClick={this.handleClickOpen(v.adress)}>
-                                        <img src={v.img} alt="" className={classes.img} />
-                                        <CardContent>
-                                            <Typography className={classes.titleCard} gutterBottom align="center">
-                                                {v.title}
-                                            </Typography>
-                                            <Typography className={classes.textCard} variant="body1" gutterBottom>
-                                                {v.subtitle}
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
-                            )
-                        })}
-                    </Grid>
-                    <AlertDialogSlide
-                        adress={this.state.adress}
-                        handleClose={this.handleClose}
-                        open={this.state.open}
-                    />
-                </Header>
-            </div>
-        )
-    }
+            {opcoesModelo.map((v, k) => {
+              return (
+                <Card key={k} className={classes.card}>
+                  <CardActionArea className={classes.cardAction} value="teste" onClick={this.handleClickOpen(v.adress)}>
+                    <img src={v.img} alt="" className={classes.img} />
+                    <CardContent>
+                      <Typography className={classes.titleCard} gutterBottom align="center">
+                        {v.title}
+                      </Typography>
+                      <Typography className={classes.textCard} variant="body1" gutterBottom>
+                        {v.subtitle}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              )
+            })}
+          </Grid>
+          <AlertDialogSlide
+            adress={this.state.adress}
+            handleClose={this.handleClose}
+            open={this.state.open}
+          />
+
+          <Dialog open={!complete} maxWidth='xl'>
+            <DialogContent>
+              {position === 0 && <Termo checked={checked} handleChange={this.handleChange}/>}
+            </DialogContent>
+            <DialogActions>
+              <Button variant='contained' color='primary' disabled={!checked}>
+                Next
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+        </Header>
+      </div>
+    )
+  }
 
 }
 EscolhaDoModelo.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
 export default withStyles(styles)(EscolhaDoModelo)
