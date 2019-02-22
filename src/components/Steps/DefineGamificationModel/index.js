ea407ca71b13843f660fdb3b0f3f101ab5bf11cf
Henrique Couto <henrique.coutu@live.com>
2019-02-22 18:00:18 -0300

    correção de erros em Define Gamification/2

--- src/components/Steps/DefineGamificationModel/index.js
+++ src/components/Steps/DefineGamificationModel/index.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-
-import GamificationType from './GamificationType'
+import GamificationType from './GamificationType';
 
 const styles = theme => ({
   title: {
@@ -15,49 +14,54 @@ const styles = theme => ({
   },
 });
 
-const name = ['participation', 'performance', 'competition', 'enjoyment', 'exploration', 'effectiveness']
+const name = [
+  'participation',
+  'performance',
+  'competition',
+  'enjoyment',
+  'exploration',
+  'effectiveness',
+];
 
 class DefineGamificationModel extends Component {
-
   state = {
-    select: ''
-  }
+    select: '',
+  };
 
-  handleSelect = event => {
-    this.setState({ select: event.currentTarget.value })
-  }
+  handleSelect = (event) => {
+    this.setState({ select: event.currentTarget.value });
+  };
 
   render() {
     const { classes } = this.props;
+    const { select } = this.state;
 
     return (
       <div style={{ justifyContent: 'center' }}>
         <Typography className={classes.title} variant="subtitle1">
-          Qual é o comportamento principal que você espera de seus alunos quando eles estão aprendendo no tutor gamificado?
+          Qual é o comportamento principal que você espera de seus alunos quando eles estão
+          aprendendo no tutor gamificado?
         </Typography>
 
-        {name.map((value, key) => {
-          return (
-            <GamificationType
-              name={value}
-              key={key}
-
-              handleSelect={this.handleSelect}
-              select={this.state.select}
-            />
-          )
-        })}
+        {name.map(value => (
+          <GamificationType
+            name={value}
+            key={value}
+            handleSelect={this.handleSelect}
+            select={select}
+          />
+        ))}
       </div>
     );
   }
 }
 
 DefineGamificationModel.propTypes = {
-  classes: PropTypes.object,
+  classes: PropTypes.object.isRequired,
 };
 
 /**
  *Quando conectado com o Redux utilizar a seguinte abordagem:
  *export default withStyles(styles)(connect(props...)(DefineGamificationModel))
-*/
+ */
 export default withStyles(styles)(DefineGamificationModel);
